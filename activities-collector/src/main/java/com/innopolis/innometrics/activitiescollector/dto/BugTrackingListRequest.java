9b474e0d27df2f8f269d78922c40523adfcd700e
Dariya <d.vakhitova@innopolis.university>
2023-04-14 01:11:06 +0300

    refactoring+tests

--- activities-collector/src/main/java/com/innopolis/innometrics/activitiescollector/dto/BugTrackingListRequest.java
+++ activities-collector/src/main/java/com/innopolis/innometrics/activitiescollector/dto/BugTrackingListRequest.java
@@ -1,10 +1,12 @@
 package com.innopolis.innometrics.activitiescollector.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class BugTrackingListRequest {
     private List<BugTrackingRequest> bugTrackingRequests;
 
